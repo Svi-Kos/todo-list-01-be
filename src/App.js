8a33de3d38800c5178d33d445677669fcc7a03cb
Svi-Kos <svitlanakos@gmail.com>
2022-12-05 18:10:47 +0200

    Container added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+// import React, { useState, useEffect } from 'react';
+// import shortid from 'shortid';
+import Container from 'components/Container';
+
 function App() {
-  return <div>Todo list will be here soon...</div>;
+  return <Container>Todo list will be here soon...</Container>;
 }
 
 export default App;
